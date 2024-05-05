fcc61fa4a35f77eb5595c263b2a0cec62b380e7f
bennynguyenemesoft <benny.nguyen@emesoft.net>
2024-05-05 16:13:07 +0700

    Initital Auth Project

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,24 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+const globalAny:any = global;
+it('responds with details about the current user', async () => {
+  const cookie = await globalAny.signin();
+
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
+});
